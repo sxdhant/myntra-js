dbffef42ccc22da4a5e2a8bdbb77c2aba8295300
sxdhant <sidhant0020@gmail.com>
2024-08-26 09:01:47 +0530

    myntra clone

---
+++ scripts/index.js
@@ -0,0 +1,80 @@
+let bagItems;
+
+onload();
+
+function onload()
+{
+    let bagItemsStr = localStorage.getItem('bagItems')
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : []
+    displayItemsOnHomePage();
+    displayBagIcon();
+}
+
+
+function displayItemsOnHomePage()
+{
+    let itemsContainerElement = document.querySelector('.items-container');
+
+    if(!itemsContainerElement)
+    {
+        return;
+    }
+    
+    let innerHtml = ''; 
+    items.forEach(item=>{
+
+        innerHtml += `
+        <div class="item-container">
+
+            <img class="item-image" src = "${item.image}" alt="item image">
+            
+            <div class="rating">${item.rating.stars} ⭐ | ${item.rating.count}k </div>
+            
+            <div class="company-name">${item.company}</div>
+            
+            <div class="item-name">${item.item_name}</div>
+            
+            <div class="price">
+                <span class="current-price">Rs ${item.current_price} </span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount">(${item.discount_percentage}% OFF)</span>
+        
+            </div>
+            
+            <div>
+                <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+        
+            </div>
+
+        </div>
+
+        `
+    });
+
+    itemsContainerElement.innerHTML = innerHtml;
+}
+
+function addToBag(itemID)
+{
+    bagItems.push(itemID);
+    localStorage.setItem('bagItems',JSON.stringify(bagItems));
+    displayBagIcon();
+
+}
+
+ function displayBagIcon()
+ {
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+
+    if(bagItems.length>0)
+    {
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerHTML = bagItems.length;
+    }
+    else{
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+
+    
+} 
+
